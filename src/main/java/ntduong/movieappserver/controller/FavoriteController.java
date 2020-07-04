eef1abb4043d3e1948b43af56497cfefbd89a405
TrieuDuong <ntduong1998vn@gmail.com>
2020-07-04 08:50:34 +0700

    update 4/7/2020

---
+++ src/main/java/ntduong/movieappserver/controller/FavoriteController.java
@@ -0,0 +1,52 @@
+/*
+ * @Author by Nguyen Trieu Duong
+ * @Email: ntduong1998vn@gmail.com
+ */
+
+package ntduong.movieappserver.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import ntduong.movieappserver.dto.ApiResponse;
+import ntduong.movieappserver.dto.FavoriteDTO;
+import ntduong.movieappserver.service.IFavoriteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@Api("FAVORITE API")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/favorite")
+public class FavoriteController {
+
+    private final IFavoriteService favoriteService;
+
+    @ApiOperation("ADD MOVIE TO FAVORITE LIST")
+    @PostMapping("/")
+    public ApiResponse<String> insertToList(@RequestBody FavoriteDTO favoriteDTO) {
+        ApiResponse<String> apiResponse = new ApiResponse<>();
+
+        try {
+
+            apiResponse.setSuccess(HttpStatus.CREATED);
+            apiResponse.setMessage("Thêm thành công.");
+        } catch (Exception e) {
+            apiResponse.setSuccess(HttpStatus.BAD_REQUEST);
+            apiResponse.setMessage(e.getMessage());
+        }
+        return apiResponse;
+    }
+
+    @ApiOperation("GET FAVORITE LIST BY USERID")
+    @GetMapping("/{userId}")
+    public ApiResponse<List<FavoriteDTO>> getList(@PathVariable int userId) {
+        ApiResponse<List<FavoriteDTO>> apiResponse = new ApiResponse<>();
+        apiResponse.setSuccess(HttpStatus.OK);
+        apiResponse.setResult(favoriteService.findAllByUserId(userId));
+        return apiResponse;
+    }
+}
--- src/main/java/ntduong/movieappserver/repository/FavoriteRepository.java
+++ src/main/java/ntduong/movieappserver/repository/FavoriteRepository.java
@@ -6,9 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface FavoriteRepository extends JpaRepository<FavoriteEntity, Integer>, JpaSpecificationExecutor<FavoriteEntity> {
-    @Override
-    <S extends FavoriteEntity> boolean exists(Example<S> example);
 
-
-    boolean existsByUserIdAndMovieId(int movieId,int userId);
 }
