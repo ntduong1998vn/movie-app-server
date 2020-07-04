64df3a7eaf1bc32d543a9c8538dabbcd6471cb9d
TrieuDuong <ntduong1998vn@gmail.com>
2020-07-05 00:12:21 +0700

    edit genreRepository

--- src/main/java/ntduong/movieappserver/repository/GenreRepository.java
+++ src/main/java/ntduong/movieappserver/repository/GenreRepository.java
@@ -1,18 +1,16 @@
 package ntduong.movieappserver.repository;
 
 import ntduong.movieappserver.entity.GenreEntity;
-import ntduong.movieappserver.repository.custom.GenreRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.SequenceGenerators;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface GenreRepository extends JpaRepository<GenreEntity, Integer>, GenreRepositoryCustom {
+public interface GenreRepository extends JpaRepository<GenreEntity, Integer> {
 
     List<GenreEntity> findByNameContainsIgnoreCase(String name);
 
--- src/main/java/ntduong/movieappserver/repository/custom/GenreRepositoryCustom.java
+++
@@ -1,6 +0,0 @@
-package ntduong.movieappserver.repository.custom;
-
-public interface GenreRepositoryCustom {
-
-    public void deleteByGenreId(int genreId);
-}
